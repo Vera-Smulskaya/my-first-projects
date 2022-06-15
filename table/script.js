30675484b37453b9e5a2f713543be00bf3d07256
Vira Smulska <vera.smulskaya@gmail.com>
2022-06-15 11:46:41 +0300

    correct visual commit

--- table/script.js
+++ table/script.js
@@ -103,13 +103,15 @@ function fillTable(data) {
             <td rowspan="2" class="table__icon">
                 <img src="${applicationIcon}" alt="icon"/>
             </td>
-            <td>${applicationName}</td>
-            <td>${typeCard}</td>
-            <td>${userName}</td>
-            <td>${dateLastTransaction}</td>
-            <td rowspan="2" class="table__status status ${classNameStatus}">
+            <td class="table__item-name">${applicationName}</td>
+            <td class="table__item-name">${typeCard}</td>
+            <td class="table__item-name">${userName}</td>
+            <td class="table__item-name">${dateLastTransaction}</td>
+            <td rowspan="2" class="${classNameStatus}">
+              <div class="table__status status">
                 <div class="status__icon"></div>
                 <div>${statusTransaction}</div>
+              </div>
             </td>
             <td rowspan="2">${dateEndTransaction}</td>
             <td rowspan="2" class="table__total-sum">${totalSum}</td>
@@ -118,7 +120,7 @@ function fillTable(data) {
             </td>
         </tr>
         <tr>
-            <td>
+            <td class="table__details">
                 <a href="//www.figma.com/" class="table__details">${applicationUrl}</a>
             </td>
             <td class="table__details">${numberCard}</td>
@@ -135,11 +137,17 @@ function fillTable(data) {
 }
 fillTable(dataTransactions);
 
+function filter(searchValue, field) {
+  const filteredData = dataTransactions.filter((item) => {
+    return item[field].toLowerCase().includes(searchValue.toLowerCase());
+  });
+  fillTable(filteredData);
+}
+
 function initFilters() {
   const elementSearch = document.getElementById("search-transaction-name");
   elementSearch.addEventListener("input", (event) => {
     filter(event.target.value, "applicationName");
-    console.log(event.target.value, "applicationName");
   });
 
   const elementUserName = document.getElementById("filter-user-name");
@@ -153,13 +161,6 @@ function initFilters() {
   });
 }
 initFilters();
-
-function filter(searchValue, field) {
-  const filteredData = dataTransactions.filter((item) => {
-    return item[field].toLowerCase().includes(searchValue.toLowerCase());
-  });
-  fillTable(filteredData);
-}
 
 function initFiltersDate() {
   const elementDateStart = document.getElementById(
@@ -192,70 +193,82 @@ function filterDate() {
     return dateStartSeconds <= dateSeconds && dateSeconds <= dataEndSeconds;
   });
   fillTable(filteredData);
-}
 
-const elementSelect = document.getElementById("sort");
-const dataTransactionsCopy = [...dataTransactions];
+  console.log(filteredData);
 
-const SORTING_OFF_CLASS = "filtres__sorting_off";
-const SORTING_ASC_CLASS = "filtres__sorting_asc";
-const SORTING_DESC_CLASS = "filtres__sorting_desc";
-
-const sortButton = document.getElementById("button-sorting");
-
-function initSelect() {
-  elementSelect.addEventListener("change", (event) => {
-    sortTable();
-  });
-
-  sortButton.addEventListener("click", () => {
-    const classList = sortButton.classList;
-    if (classList.contains(SORTING_OFF_CLASS)) {
-      classList.replace(SORTING_OFF_CLASS, SORTING_ASC_CLASS);
-      sortTable(true);
-    } else if (classList.contains(SORTING_ASC_CLASS)) {
-      classList.replace(SORTING_ASC_CLASS, SORTING_DESC_CLASS);
-      sortTable(false);
-    } else {
-      classList.replace(SORTING_DESC_CLASS, SORTING_OFF_CLASS);
-      sortTable();
-    }
-  });
-}
-initSelect();
-
-function sortTable(isAsc) {
-  const sortField = elementSelect.value;
-
-  if (isAsc === undefined) {
-    fillTable(dataTransactions);
-
-    return;
+  if (filteredData.length === 0) {
+    const tableData = document.querySelector("tbody");
+    tableData.innerHTML = "<div>no data</div>";
   }
-
-  dataTransactionsCopy.sort((a, b) => {
-    if (a[sortField] === b[sortField]) return 0;
-
-    let result;
-
-    if (
-      sortField === "dateLastTransaction" ||
-      sortField === "dateEndTransaction"
-    ) {
-      const aDateSeconds = new Date(a[sortField]).getTime();
-      const bDateSeconds = new Date(b[sortField]).getTime();
-
-      result = aDateSeconds > bDateSeconds ? 1 : -1;
-    } else {
-      result = a[sortField] > b[sortField] ? 1 : -1;
-    }
-    
-    if (!isAsc) {
-      result = -result;
-    }
-
-    return result;
-  });
-
-  fillTable(dataTransactionsCopy);
 }
+
+(() => {
+  const ELEMENT_SELECT = document.getElementById("sort");
+  const DATA_TRANSACTIONS_COPY = [...dataTransactions];
+  const SORTING_OFF_CLASS = "filtres__sorting_off";
+  const SORTING_ASC_CLASS = "filtres__sorting_asc";
+  const SORTING_DESC_CLASS = "filtres__sorting_desc";
+  const SORT_BUTTON = document.getElementById("button-sorting");
+  const SORT_DIRECTION = {
+    ASC: 0,
+    DSC: 1,
+    DEFAULT: 2,
+  };
+
+  function initSelect() {
+    ELEMENT_SELECT.addEventListener("change", () => {
+      sortTable();
+    });
+
+    SORT_BUTTON.addEventListener("click", () => {
+      const classList = SORT_BUTTON.classList;
+      if (classList.contains(SORTING_OFF_CLASS)) {
+        classList.replace(SORTING_OFF_CLASS, SORTING_ASC_CLASS);
+        sortTable(SORT_DIRECTION.ASC);
+      } else if (classList.contains(SORTING_ASC_CLASS)) {
+        classList.replace(SORTING_ASC_CLASS, SORTING_DESC_CLASS);
+        sortTable(SORT_DIRECTION.DSC);
+      } else {
+        classList.replace(SORTING_DESC_CLASS, SORTING_OFF_CLASS);
+        sortTable(SORT_DIRECTION.DEFAULT);
+      }
+    });
+  }
+  initSelect();
+
+  function sortTable(sortDirection) {
+    const sortField = ELEMENT_SELECT.value;
+
+    if (sortDirection === SORT_DIRECTION.DEFAULT) {
+      fillTable(dataTransactions);
+
+      return;
+    }
+
+    DATA_TRANSACTIONS_COPY.sort((a, b) => {
+      if (a[sortField] === b[sortField]) return 0;
+
+      let result;
+
+      if (
+        sortField === "dateLastTransaction" ||
+        sortField === "dateEndTransaction"
+      ) {
+        const aDateSeconds = new Date(a[sortField]).getTime();
+        const bDateSeconds = new Date(b[sortField]).getTime();
+
+        result = aDateSeconds > bDateSeconds ? 1 : -1;
+      } else {
+        result = a[sortField] > b[sortField] ? 1 : -1;
+      }
+
+      if (sortDirection === SORT_DIRECTION.DSC) {
+        result = -result;
+      }
+
+      return result;
+    });
+
+    fillTable(DATA_TRANSACTIONS_COPY);
+  }
+})();
