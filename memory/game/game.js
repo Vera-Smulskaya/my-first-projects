51cc9706e3ab06917094e884ed9c8e7bf4f98bc4
Vira Smulska <vera.smulskaya@gmail.com>
2022-06-22 17:41:34 +0300

    game start

---
+++ memory/game/game.js
@@ -0,0 +1,30 @@
+// class MyClass {
+//     // методы класса
+//     constructor() { ... }
+//     method1() { ... }
+//     method2() { ... }
+//     method3() { ... }
+//     ...
+//   } 
+
+//   class User {
+
+//     constructor(name) {
+//       this.name = name;
+//     }
+  
+//     sayHi() {
+//       alert(this.name);
+//     }
+  
+//   }
+  
+class Card {
+
+    // constructor
+}
+
+
+  const cards = document.querySelectorAll('.cards__element');
+
+  console.log('CARDS', cards);
