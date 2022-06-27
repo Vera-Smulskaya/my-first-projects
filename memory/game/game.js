6103689120ab7205a503023341326e52f76a6ae0
Vira Smulska <vera.smulskaya@gmail.com>
2022-06-27 15:55:55 +0300

    timer

--- memory/game/game.js
+++ memory/game/game.js
@@ -6,54 +6,106 @@
 //       }
 // }
 
-// class Field {
+// const arrLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'];
 
-// }
-const arrLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'];
+// const arrCards = [...arrLetters, ...arrLetters];
 
-const arrCards = [...arrLetters, ...arrLetters];
+// function shuffle(array) {
+//     array.sort(() => Math.random() - 0.5);
+//     console.log(array);
+//   }
+//   shuffle(arrCards);
 
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-    console.log(array);
-  }
-  shuffle(arrCards); 
- 
-  function renderCard(letter) {
-    return `<div class="cards__element">${letter}</div>`          
-  } 
+//   function renderCard(letter) {
+//     return `<div class="cards__front">${letter}</div>`
+//   }
 
- function renderCards() {
- const listElement = document.getElementById('card-list');
- const cardsHtml = arrCards.map(renderCard).join('');
- listElement.innerHTML = cardsHtml;
- }
- renderCards();
+//  function renderCards() {
+//  const listElement = document.getElementById('card-list');
+//  const cardsHtml = arrCards.map(renderCard).join('');
+//  listElement.innerHTML = cardsHtml;
+//  }
+//  renderCards();
 
-
-// _getElement() {
-//     const cardElement = document.querySelector(this._cardSelector).content.querySelector('.cards__element');
-//     return cardElement;
-// }
-
-
-// const cards = document.querySelectorAll('.cards__element');
-// console.log(cards);
-
-// const arrCard = Array.from(document.querySelectorAll('.cards__element'))
-// console.log(arrCard);
-
-// arrCard.forEach(card => {
+// arrCards.forEach(card => {
 //         card.addEventListener("click", flipCard);
 // })
 
 // function flipCard() {
-//     document.getElementById("back").style.display = "none";
-//     document.getElementById("front").style.display = "flex";
+//     // document.getElementById("back").style.display = "none";
+//     // document.getElementById("front").style.display = "flex";
 //     }
 // flipCard();
 
-// (document.getElementById("back").style.display = "none") && (document.getElementById("front").style.display = "flex");
+class Game {
+  constructor(timer) {
+    this._timer = timer;
+  }
+  createCards() {
+    const letters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+    ];
+    const cardLetters = [...letters, ...letters];
+    this.shuffle(cardLetters);
+  }
 
+  shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    console.log(array);
+  }
+}
 
+class Timer {
+  constructor() {
+    this._startTime = null;
+    this._stopTime = null;
+  }
+
+  get time() {
+    if (!this._startTime) {
+      return 0;
+    } else if (this._startTime && !this._stopTime) {
+      return Date.now() - this._startTime;
+    } else if (this._stopTime) {
+      return this._stopTime - this._startTime;
+    }
+  }
+
+  start() {
+    this._stopTime = null;
+    this._startTime = Date.now();
+  }
+
+  stop() {
+    if(this._startTime) {
+    this._stopTime = Date.now();
+  }
+  }
+
+  reset() {
+    this._startTime = null;
+    this._stopTime = null;
+  }
+}
+
+const timer = new Timer();
+const game = new Game(timer);
+game.createCards();
 
