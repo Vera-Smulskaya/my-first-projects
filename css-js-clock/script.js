863ac1b4c2dd17d5f4b99799274ff8f563e3c9e8
Vira Smulska <vera.smulskaya@gmail.com>
2022-07-26 13:35:50 +0300

    my projects

---
+++ css-js-clock/script.js
@@ -0,0 +1,26 @@
+(() => {
+  const HOURS_BLOCK = "clock__hours";
+  const MINUTES_BLOCK = "clock__minutes";
+  const SECONDS_BLOCK = "clock__seconds";
+
+  function startClock() {
+    const hoursArrow = document.querySelector(`.${HOURS_BLOCK}`);
+    const minutesArrow = document.querySelector(`.${MINUTES_BLOCK}`);
+    const secondsArrow = document.querySelector(`.${SECONDS_BLOCK}`);
+    const deg = 6;
+
+    setInterval(() => {
+      const day = new Date();
+
+      const hours = day.getHours() * 30;
+      const minutes = day.getMinutes() * deg;
+      const seconds = day.getSeconds() * deg;
+
+      hoursArrow.style.transform = `rotateZ(${hours + minutes / 12}deg)`;
+      minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+      secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+    }, 0);
+  }
+
+  startClock();
+})();
